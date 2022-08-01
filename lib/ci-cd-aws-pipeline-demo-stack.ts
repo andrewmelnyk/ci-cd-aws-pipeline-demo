331fd2b5fa513aa9738ffdf7f24270a2dcd16807
andrewmelnyk <melnyk.andrew@gmail.com>
2022-07-31 21:58:49 -0400

    initial

---
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -0,0 +1,25 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CiCdAwsPipelineDemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "TestPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "andrewmelnyk/ci-cd-aws-pipeline-demo",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+  }
+}
